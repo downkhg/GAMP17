ff6eb245f59abcd4001d57d63c98e146b3499d2d
khg <dowkhg@gmail.com>
2021-01-22 19:34:07 +0900

    교환법칙과 프랜즈연산자

--- CPP_STUDY/Vector/Vector.cpp
+++ CPP_STUDY/Vector/Vector.cpp
@@ -33,6 +33,30 @@ public:
 		CVector temp(this->x * dist, this->y * dist);
 		return temp;
 	}
+	friend CVector operator*(float dist, CVector v)//½ºÄ®¶ó°ö(±³È¯¹ýÄ¢)
+	{
+		return CVector(v.x * dist, v.y * dist);
+	}
+	float operator[](int idx)
+	{
+		if (idx == 0) return x;
+		else return y;
+	}
+	float operator[](char c)
+	{
+		if (c == 'x') return x;
+		else if(c == 'y') return y;
+		return 0;
+	}
+	float Magnitude()
+	{
+		return sqrt(x*x + y * y);
+	}
+	CVector Normarlize()
+	{
+		float fDist = Magnitude();
+		return CVector(x / fDist, y / fDist);
+	}
 	void Print(const char* msg)
 	{
 		cout << msg << "(" << x << ","<< y << ")" << endl;
@@ -41,12 +65,29 @@ public:
 
 void VectorMain()
 {
-	CVector vA(0,0);
-	CVector vB(1,1);
-	//´õÇÏ±â,»©±â,°öÇÏ±â
+	CVector vPA(0,0);
+	CVector vA(10,1);
+	vPA.Print("PA");
+	vA.Print("A");
+	//´õÇÏ±â
+	CVector vPB = vPA + vA;
+	vPB.Print("PB");
+	//»©±â
+	vA = vPB - vPA;
+	vA.Print("A");
+	vA = vPA - vPB;
+	vA.Print("A");
+	CVector vDir = vA.Normarlize();
+	float fDist = vA.Magnitude();
+	vDir.Print("Dir");
+	cout << "Dist:" << fDist << endl;
+	//°öÇÏ±â
+	vA.Print("A");
+	vA = fDist * vDir;
+	vA.Print("A");
 }
 
 void main()
 {
-
+	VectorMain();
 }
