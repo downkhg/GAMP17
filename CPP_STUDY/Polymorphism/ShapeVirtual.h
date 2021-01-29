7d4d87a98bebb20647a6036467eedd3501b2ecf5
downkhg <downkhg@gmail.com>
2021-01-29 19:24:12 +0900

    가상함수, 순수가상함수, 추상클래스,인터페이스

--- CPP_STUDY/Polymorphism/ShapeVirtual.h
+++ CPP_STUDY/Polymorphism/ShapeVirtual.h
@@ -18,6 +18,9 @@ namespace Virtual
 		}
 	};
 
+	
+
+	//순수가상함수를 1개이상 가진함수를 추상클래스라고한다. 객체화 불가능함.
 	class CShape
 	{
 	public:
@@ -25,12 +28,28 @@ namespace Virtual
 		{
 			cout << "CShape:" << this << endl;
 		}
-		~CShape()
+		//소멸자도 함수이므로 부모에서 호출하기위해서 virtual을 사용해야함.
+		//소멸자는 기본소멸자가 있으므로 순수가상소멸자를 만들수없다.
+		virtual ~CShape() //= 0
 		{
 			cout << "~CShape:" << this << endl;
 		}
 		//가상함수: 부모의 포인터에서도 자식의 객체 함수를 호출하는것.
-		virtual void Draw()
+		//순수가상함수: 상속받은 객체가 반드시 오버라이드해야하는 함수
+		virtual void Draw() = 0
+		{
+			cout << "CShape::Draw:" << this << endl;
+		}
+	};
+
+	//C#,Java에서는 다중상속은 인터페이스만 가능하다.
+	//※멤버변수가 없으므로, 변수가 겹치는 일이 없다.
+	//인터페이스: 멤버변수를 구현할수없는 추상클래스.
+	__interface IShape
+	{
+		//SPoint sPoint; //멤버변수를 가질수없다.
+	public:
+		virtual void Draw() = 0
 		{
 			cout << "CShape::Draw:" << this << endl;
 		}
@@ -52,6 +71,8 @@ namespace Virtual
 			cout << "~CRect:" << this << endl;
 		}
 		//오버라이딩: 부모가 있는 함수와 같은이름의 가상함수를 재정의하는 것
+		//순수가상함수는 반드시 재정의 해야한다.
+		//재정의하지않으면 객체를 만들때 오류가 발생한다.
 		void Draw() override
 		{
 			cout << "CRect::Draw()" << endl;
@@ -108,13 +129,14 @@ namespace Virtual
 
 	void ShapeTestMain()
 	{
-		CShape cShape;//형태라는 도형은 없음. 상위분류임.
+		//CShape cShape;//형태라는 도형은 없음. 상위분류임.
+		CShape* pShape; //포인터는 실체가 없으므로 가능하다.
 		CRect cRect;
 		CCircle cCircle;
 		CTriangle cTriangle;
 		//정적바인딩: 메모리가 할당될때 함수의 기능이 결정됨.
-		cShape.Draw();
-		cRect.Draw();
+		//cShape.Draw();
+		//cRect.Draw();
 		cCircle.Draw();
 		cTriangle.Draw();
 	}
