a78db959fa124009710bf3bcb6c5048fb8892a74
downkhg <downkhg@gmail.com>
2021-03-25 19:44:34 +0900

    빌보드와 이펙트

---
+++ ComputerGraphics/ComputerGraphics/Assets/Scripts/Billboard.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+    public Camera m_cameraTarget;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(m_cameraTarget.transform);
+    }
+}
