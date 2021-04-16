37cfcbb776ca4a1a1ce913e665e807a71d68f1fb
downkhg <downkhg@gmail.com>
2021-04-16 20:24:43 +0900

    uGUI

---
+++ GameProgramming/RoomExitUnity/Assets/Scprits/GUI/GUIStatusBar.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GUIStatusBar : MonoBehaviour
+{
+    public void SetStatus(float cur, float max)
+    {
+        float fRat = cur / max;
+        RectTransform rect = GetComponent<RectTransform>();
+        Vector2 vSize = rect.sizeDelta;
+        vSize.x *= fRat;
+        rect.sizeDelta = vSize;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
